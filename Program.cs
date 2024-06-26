af80df00c925330fbf5b86d75d19d1511936bdd2
MusingaBrian <musingabrians@gmail.com>
2024-06-26 11:27:15 +0300

    Final Commit

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿/*  This is a multiline comment
+﻿/*-----------------------------------------------*/
+/*  This is a multiline comment
 string firstName = "Bob";
 int widgetsPurchased = 7;
 // Testing a change to the message.
@@ -7,12 +8,22 @@ int widgetsPurchased = 7;
 Console.WriteLine($"{firstName} purchased {widgetsPurchased} widgets.");
 */
 
+/*-----------------------------------------------*/
+
+
+
 /*
   The following code creates five random OrderIDs
   to test the fraud detection process.  OrderIDs 
   consist of a letter from A to E, and a three
   digit number. Ex. A123.
 */
+
+/*-----------------------------------------------*/
+
+
+
+/*
 Random random = new Random();
 
 string[] orderIDs = new string[5];
@@ -33,3 +44,36 @@ foreach (string orderID in orderIDs)
 {
     Console.WriteLine(orderID);
 }
+*/
+
+
+/*-----------------------------------------------*/
+
+/*
+   This code reverses a message, counts the number of times 
+   a particular character appears, then prints the results
+   to the console window.
+ */
+
+string str = "The quick brown fox jumps over the lazy dog.";
+
+// convert the message into a char array
+char[] charMessage = str.ToCharArray();
+
+// Reverse the chars
+Array.Reverse(charMessage);
+
+int x = 0;
+
+// count the o's
+foreach (char i in charMessage)
+{
+    if (i == 'o') x++;
+}
+
+// convert it back to a string
+string new_message = new String(charMessage);
+
+// print it out
+Console.WriteLine(new_message);
+Console.WriteLine($"'o' appears {x} times.");
